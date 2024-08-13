9307f0495a152e466a65f82155a1c170fc8b1f94
zyzzchad <119152524+gautamalish@users.noreply.github.com>
2024-08-13 07:22:26 +0545

    Added some tailwind classes for mobile responsiveness

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -7,8 +7,8 @@ const List = () => {
       <div className="m-auto p-3 w-fit">
         <h3 className="text-2xl font-bold text-red-400">Your Todos</h3>
       </div>
-      <div className="bg-green-200 w-2/5 m-auto h-4/5 p-4 overflow-auto">
-        <div className="w-3/4 m-auto bg-blue-400 p-3 rounded-md flex justify-between items-center">
+      <div className="bg-green-200 w-2/5 m-auto h-4/5 p-4 overflow-auto max-sm:w-4/5">
+        <div className="w-11/12 m-auto bg-blue-400 p-3 rounded-md flex justify-between items-center max-sm:w-full">
           <div>
             <h3 className="text-xl">Title</h3>
             <p>Description</p>
--- client/src/pages/CreateTodo.jsx
+++ client/src/pages/CreateTodo.jsx
@@ -1,12 +1,20 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 const CreateTodo = ({ setDisplayForm, displayForm }) => {
   const modelRef = useRef(null);
+  const [formData, setFormData] = useState({ title: "", description: "" });
   const OnClickOutside = (e) => {
     if (!modelRef.current.contains(e.target)) {
       setDisplayForm(false);
     }
   };
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
+  console.log(formData);
   useEffect(() => {
     document.addEventListener("mousedown", OnClickOutside);
 
@@ -19,8 +27,22 @@ const CreateTodo = ({ setDisplayForm, displayForm }) => {
       className="absolute m-auto inset-0 bg-indigo-400 w-2/6 h-3/5 flex flex-col p-3 gap-3 rounded-xl"
       ref={modelRef}
     >
-      <input type="text" placeholder="Title" className="p-3 rounded-md" />
-      <input type="text" placeholder="Description" className="p-3 rounded-md" />
+      <input
+        type="text"
+        placeholder="Title"
+        className="p-3 rounded-md"
+        value={formData.title}
+        onChange={handleChange}
+        name="title"
+      />
+      <input
+        type="text"
+        placeholder="Description"
+        className="p-3 rounded-md"
+        value={formData.description}
+        onChange={handleChange}
+        name="description"
+      />
       <button
         onClick={() => setDisplayForm(false)}
         className="bg-red-200 p-2 rounded-md hover:bg-red-500 hover:text-white duration-200"
