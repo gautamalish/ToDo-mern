7f424355d3c68c6b14bec75f7e92a37f6042c4c4
zyzzchad <119152524+gautamalish@users.noreply.github.com>
2024-08-13 19:12:16 +0545

    Created api routes for deleting and updating the list items

--- api/controllers/list.controller.js
+++ api/controllers/list.controller.js
@@ -1,5 +1,4 @@
 import ListModel from "../models/list.model.js";
-
 export const GetLists = async (req, res) => {
   try {
     const result = await ListModel.find();
@@ -15,6 +14,42 @@ export const PostList = async (req, res) => {
     const newItem = new ListModel({ title, description });
     await newItem.save();
     res.status(201).json({ newItem });
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+export const DeleteList = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const item = await ListModel.findByIdAndDelete({ _id: id });
+    if (item) {
+      res.status(200).json({ message: "Item deleted successfully." });
+    } else {
+      res.status(404).json({ message: "List not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const UpdateList = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const item = req.body;
+    if (!item || Object.keys(item).length === 0) {
+      return res
+        .status(400)
+        .json({ message: "Please provide data to update." });
+    }
+    const updateItem = await ListModel.findByIdAndUpdate(id, item, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updateItem) {
+      return res.status(404).json({ message: "Not Found" });
+    }
+    res.status(200).json(updateItem);
   } catch (err) {
     res.status(500).json(err.message);
   }
--- api/routes/list.route.js
+++ api/routes/list.route.js
@@ -1,7 +1,14 @@
 import { Router } from "express";
-import { GetLists, PostList } from "../controllers/list.controller.js";
+import {
+  DeleteList,
+  GetLists,
+  PostList,
+  UpdateList,
+} from "../controllers/list.controller.js";
 const router = Router();
 
 router.get("/", GetLists);
 router.post("/add", PostList);
+router.delete("/delete/:id", DeleteList);
+router.patch("/update/:id", UpdateList);
 export default router;
